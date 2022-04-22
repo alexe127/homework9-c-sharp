9b0c73b5dbc3ac9c4d77c618ed9bdfd16fcb45ef
«Alex» <aser_e@mail.ru>
2022-04-22 23:33:18 +0900

    zadacha4

---
+++ zadacha4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу вычисления функции Аккермана
+
+int Akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return Akkerman(m - 1, 1);
+    else return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+Console.WriteLine();
+
+int M = 3;
+int N = 2;
+Console.WriteLine($"Значение функции Аккермана = {Akkerman(M, N)}");
