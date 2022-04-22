1888db57ffd6e386e254d73342305e72a43ef3f7
«Alex» <aser_e@mail.ru>
2022-04-22 23:32:46 +0900

    zadacha2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Написать программу возведения числа А в целую степень B.
+
+// Console.WriteLine("Введите число: ");
+// int a = int.Parse(Console.ReadLine() ?? "");
+// Console.WriteLine("Введите число: ");
+// int b = int.Parse(Console.ReadLine() ?? "");
+
+Random rnd = new Random();
+int a = rnd.Next(1,10);
+int b = rnd.Next(1,5);
+int result = 1;
+
+void Recurs(int n)
+{
+    if (n == 0) return;
+    result *= a;
+    Recurs(n - 1);
+}
+
+Recurs(b);
+Console.WriteLine($"{a}  в степени {b} =  {result}");
