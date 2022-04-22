990943f1c59d043ff11180fef894cd413a8acd75
«Alex» <aser_e@mail.ru>
2022-04-22 23:32:19 +0900

    zadacha1

---
+++ zadacha1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти сумму цифр числа
+
+int sum =0;
+void sumNum(int n )
+{
+    if (n == 0) return;        
+    sum = sum + n % 10;
+    sumNum( n / 10);
+}
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine() ?? "");
+sumNum(num);
+Console.WriteLine($"Сумма цифр в числе = {sum} ");
