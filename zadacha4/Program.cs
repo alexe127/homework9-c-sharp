850680afe22b278650b88892dd0a3b86d388e1a7
«Alex» <aser_e@mail.ru>
2022-05-07 21:58:24 +0900

    zadacha4 corrected

--- zadacha4/Program.cs
+++ zadacha4/Program.cs
@@ -1,13 +1,14 @@
 ﻿// Написать программу вычисления функции Аккермана
 
-int Akkerman(int m, int n)
+int Akkerman(int a, int b)
 {
-    if (m == 0) return n + 1;
-    else if (n == 0) return Akkerman(m - 1, 1);
-    else return Akkerman(m - 1, Akkerman(m, n - 1));
+    if (a == 0) return b + 1;
+    else if (b == 0) return Akkerman(a - 1, 1);
+    else return Akkerman(a - 1, Akkerman(a, b - 1));
 }
-Console.WriteLine();
 
-int M = 3;
-int N = 2;
-Console.WriteLine($"Значение функции Аккермана = {Akkerman(M, N)}");
+Console.Write("Введите число 1: ");
+int A = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число 2: ");
+int B = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"Значение функции Аккермана = {Akkerman(A, B)}");
